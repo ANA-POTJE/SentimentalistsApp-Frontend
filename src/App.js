952774a2ff5656cf452fbc40389d5b34ffdceacf
Ellie Maidment <ellie947@hotmail.com>
2020-10-20 09:54:32 +0100

    import useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Container, Grid } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
@@ -31,6 +31,7 @@ const classes = useStyles();
 
  //request
  const [request, setRequest] = useState("");
+ useEffect()
  
  
 
