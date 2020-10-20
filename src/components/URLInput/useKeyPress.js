3fe54b2a64543e68f60f0fd132cc913322498239
Ellie Maidment <ellie947@hotmail.com>
2020-10-20 11:55:36 +0100

    implement event listeners

--- src/components/URLInput/useKeyPress.js
+++ src/components/URLInput/useKeyPress.js
@@ -3,5 +3,16 @@ import { useEffect } from 'react';
 
 
 export default function useKeyPress(key, action){
-    //to be implemented
+    useEffect(() => {
+    // TODO: define event listener
+    function onKeyup(e) {
+        if (e.key === key) action()
+      }
+
+    // TODO: register event listener
+    window.addEventListener('keyup', onKeyup);
+    // TODO: unregister event listener
+    return () => window.removeEventListener('keyup', onKeyup);
+    // TODO: link to component lifecycle
+    }, [])
 }
